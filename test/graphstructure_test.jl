d4ecb21dc01695618336425c31412eaf501535f2
MarilouF <farmer.marilou@hotmail.fr>
2020-07-14 12:28:37 -0400

    Graph Structure tests

--- test/graphstructure_test.jl
+++ test/graphstructure_test.jl
@@ -1,11 +1,16 @@
 @testset "graphstructure.jl" begin
+    gs = GMRF.GraphStructure(2, [[2], [1]])
+    io = IOBuffer()
+
     @testset "showGraphStructure(io, obj; prefix)" begin
-        # TODO
+        # does not throw
+        @test_logs GMRF.showGraphStructure(io, gs, prefix = "\t")
 
     end
 
     @testset "Base.show(io, obj)" begin
-        # TODO
+        # does not throw
+        @test_logs Base.show(io, gs)
 
     end
 
