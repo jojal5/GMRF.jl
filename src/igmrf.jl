071e306a741cf322d490da069f56a5bbad537699
MarilouF <farmer.marilou@hotmail.fr>
2020-07-14 11:01:31 -0400

    Split iGMRF constructor into smaller functions

--- src/igmrf.jl
+++ src/igmrf.jl
@@ -17,160 +17,164 @@ end
 
 function iGMRF(m₁::Integer, m₂::Integer, order::Integer, κ::Real)::iGMRF
 
-#=Gives the adjacency matrix W for the iGMRF of order 1 or 2 on the regular
-grid of size (m1 * m2). =#
-
-    m = m₁*m₂
+    #=Gives the adjacency matrix W for the iGMRF of order 1 or 2 on the regular
+    grid of size (m1 * m2). =#
 
     if order == 1
 
-        # 1-off diagonal elements
-        v = ones(Int64,m₁)
-        v[end] = 0
-        V = repeat(v,outer=m₂)
-        pop!(V)
+        nbs, W = fo_nbs(m₁, m₂)
+        condIndSubset = fo_condindsubsets(m₁, m₂)
 
-        # n-off diagonal elements
-        U = ones(Int64,m₁*(m₂-1))
+    elseif order == 2
 
-        # get the upper triangular part of the matrix
-        D = sparse(1:(m-1), 2:m, V, m, m) + sparse(1:(m-m₁),(m₁+1):m, U, m, m)
-
-        # make D symmetric
-        D = D + D'
-
-        # Compute the list of neighbors for each node
-        nbs = fill(Int[], m)
-        for i = 1:m
-            nbs[i] = findall(!iszero, D[:,i])
-        end
-
-        nnbs = length.(nbs)
-
-        # Put the number of neighbors on the diagonal
-        W = -D + spdiagm(0 => nnbs)
-
-
-    elseif order==2
-
-        # Alternative by adding molecules. There should not be missing values in the grid.
-
-        W = spzeros(Int64,m,m)
-        pos = reshape(1:m,m₁,m₂)
-
-        for i=1:m₁
-            for j=1:m₂
-
-                S = zeros(Int64,m₁,m₂)
-
-                if (i-2>0)
-                   S[i-2:i,j] =  S[i-2:i,j] + [1, -2, 1]
-                end
-
-                if (i+2<=m₁)
-                   S[i:i+2,j] =  S[i:i+2,j] + [1, -2, 1]
-                end
-
-                if (j-2>0)
-                   S[i,j-2:j] =  S[i,j-2:j] + [1,-2, 1]
-                end
-
-                if (j+2<=m₂)
-                   S[i,j:j+2] =  S[i,j:j+2] + [1,-2, 1]
-                end
-
-
-
-                if (i-1>0) && (i+1<=m₁)
-                    S[i-1:i+1,j] = S[i-1:i+1,j] + [-2, 4, -2]
-                end
-
-                if (j-1>0) && (j+1<=m₂)
-                    S[i,j-1:j+1] = S[i,j-1:j+1] + [-2, 4, -2]
-                end
-
-
-
-                if (i-1>0) && (j+1<=m₂)
-                    S[i-1:i,j:j+1] = S[i-1:i,j:j+1] + [-2 2; 2 -2]
-                end
-
-                if (i+1<=m₁) && (j+1<=m₂)
-                    S[i:i+1,j:j+1] = S[i:i+1,j:j+1] + [2 -2; -2 2]
-                end
-
-                if (i-1>0) && (j-1>0)
-                    S[i-1:i,j-1:j] = S[i-1:i,j-1:j] + [2 -2; -2 2]
-                end
-
-                if (i+1<=m₁) && (j-1>0)
-                    S[i:i+1,j-1:j] = S[i:i+1,j-1:j] + [-2 2; 2 -2]
-                end
-
-                W[:,pos[i,j]] = S[:]
-
-            end
-        end
-
-        # Compute the list of neighbors for each node
-        nbs =  Array{Int64,1}[]
-        for i = 1:m
-            push!(nbs,findall(W[:,i] .< 0))
-        end
-
-        nnbs = length.(nbs)
+        nbs, W = so_nbs(m₁, m₂)
+        condIndSubset = so_condindsubsets(m₁, m₂)
 
     end
 
-    condIndSubset = condindsubsets(m₁,m₂,order)
+    W̄ = W - sparse(diagm(length.(nbs)))
 
-    W̄ = W - sparse(diagm(nnbs))
-
-    G = GridStructure((m₁,m₂), nbs, condIndSubset,W,W̄)
+    G = GridStructure((m₁, m₂), nbs, condIndSubset, W, W̄)
 
     return iGMRF(G, order, κ)
 
 end
 
-function condindsubsets(m₁::Integer,m₂::Integer,order::Integer)::Vector{Vector{Integer}}
+function fo_nbs(m₁::Integer, m₂::Integer)::Tuple{Vector{Vector{Int64}}, SparseMatrixCSC{Int64,Int64}}
 
-    if order == 1
+    # 1-off diagonal elements
+    v = ones(Int64,m₁)
+    v[end] = 0
+    V = repeat(v,outer=m₂)
+    pop!(V)
 
-        condIndSubsetIndex = 2*ones(Int64,m₁,m₂)
-        condIndSubsetIndex[1:2:end,1:2:end] .= 1
-        condIndSubsetIndex[2:2:end,2:2:end] .= 1
+    # n-off diagonal elements
+    U = ones(Int64,m₁*(m₂-1))
 
-        condIndSubset = Array[findall(vec(condIndSubsetIndex) .==i) for i=1:2]
+    # get the upper triangular part of the matrix
+    m = m₁ * m₂
+    D = sparse(1:(m-1), 2:m, V, m, m) + sparse(1:(m-m₁),(m₁+1):m, U, m, m)
 
-    elseif order == 2
+    # make D symmetric
+    D = D + D'
 
-        condIndSubsetIndex = zeros(Int64,m₁,m₂)
-
-        condIndSubsetIndex[1:3:end,1:4:end] .= 1
-        condIndSubsetIndex[2:3:end,3:4:end] .= 1
-
-        condIndSubsetIndex[1:3:end,2:4:end] .= 2
-        condIndSubsetIndex[2:3:end,4:4:end] .= 2
-
-        condIndSubsetIndex[1:3:end,3:4:end] .= 3
-        condIndSubsetIndex[2:3:end,1:4:end] .= 3
-
-        condIndSubsetIndex[1:3:end,4:4:end] .= 4
-        condIndSubsetIndex[2:3:end,2:4:end] .= 4
-
-        condIndSubsetIndex[3:3:end,1:3:end] .= 5
-        condIndSubsetIndex[3:3:end,2:3:end] .= 6
-        condIndSubsetIndex[3:3:end,3:3:end] .= 7
-
-        condIndSubset = Array[findall(vec(condIndSubsetIndex) .==i) for i=1:7]
-
+    # Compute the list of neighbors for each node
+    nbs = fill(Int[], m)
+    for i = 1:m
+        nbs[i] = findall(!iszero, D[:,i])
     end
 
-    return condIndSubset
+    # Put the number of neighbors on the diagonal
+    W = -D + spdiagm(0 => length.(nbs))
+
+    return (nbs, W)
 
 end
 
+function so_nbs(m₁::Integer, m₂::Integer)::Tuple{Vector{Vector{Int64}}, SparseMatrixCSC{Int64,Int64}}
+    # Alternative by adding molecules. There should not be missing values in the grid.
+    m = m₁ * m₂
+    W = spzeros(Int64,m,m)
+    pos = reshape(1:m,m₁,m₂)
 
+    for i=1:m₁
+        for j=1:m₂
+
+            S = zeros(Int64,m₁,m₂)
+
+            if (i-2>0)
+               S[i-2:i,j] =  S[i-2:i,j] + [1, -2, 1]
+            end
+
+            if (i+2<=m₁)
+               S[i:i+2,j] =  S[i:i+2,j] + [1, -2, 1]
+            end
+
+            if (j-2>0)
+               S[i,j-2:j] =  S[i,j-2:j] + [1,-2, 1]
+            end
+
+            if (j+2<=m₂)
+               S[i,j:j+2] =  S[i,j:j+2] + [1,-2, 1]
+            end
+
+
+
+            if (i-1>0) && (i+1<=m₁)
+                S[i-1:i+1,j] = S[i-1:i+1,j] + [-2, 4, -2]
+            end
+
+            if (j-1>0) && (j+1<=m₂)
+                S[i,j-1:j+1] = S[i,j-1:j+1] + [-2, 4, -2]
+            end
+
+
+
+            if (i-1>0) && (j+1<=m₂)
+                S[i-1:i,j:j+1] = S[i-1:i,j:j+1] + [-2 2; 2 -2]
+            end
+
+            if (i+1<=m₁) && (j+1<=m₂)
+                S[i:i+1,j:j+1] = S[i:i+1,j:j+1] + [2 -2; -2 2]
+            end
+
+            if (i-1>0) && (j-1>0)
+                S[i-1:i,j-1:j] = S[i-1:i,j-1:j] + [2 -2; -2 2]
+            end
+
+            if (i+1<=m₁) && (j-1>0)
+                S[i:i+1,j-1:j] = S[i:i+1,j-1:j] + [-2 2; 2 -2]
+            end
+
+            W[:,pos[i,j]] = S[:]
+
+        end
+    end
+
+    # Compute the list of neighbors for each node
+    nbs =  Array{Int64,1}[]
+    for i = 1:m
+        push!(nbs,findall(W[:,i] .< 0))
+    end
+
+    return (nbs, W)
+
+end
+
+function fo_condindsubsets(m₁::Integer,m₂::Integer)::Vector{Vector{Integer}}
+
+
+    condIndSubsetIndex = 2*ones(Int64,m₁,m₂)
+    condIndSubsetIndex[1:2:end,1:2:end] .= 1
+    condIndSubsetIndex[2:2:end,2:2:end] .= 1
+
+    return Array[findall(vec(condIndSubsetIndex) .==i) for i=1:2]
+
+end
+
+function so_condindsubsets(m₁::Integer,m₂::Integer)::Vector{Vector{Integer}}
+
+    condIndSubsetIndex = zeros(Int64,m₁,m₂)
+
+    condIndSubsetIndex[1:3:end,1:4:end] .= 1
+    condIndSubsetIndex[2:3:end,3:4:end] .= 1
+
+    condIndSubsetIndex[1:3:end,2:4:end] .= 2
+    condIndSubsetIndex[2:3:end,4:4:end] .= 2
+
+    condIndSubsetIndex[1:3:end,3:4:end] .= 3
+    condIndSubsetIndex[2:3:end,1:4:end] .= 3
+
+    condIndSubsetIndex[1:3:end,4:4:end] .= 4
+    condIndSubsetIndex[2:3:end,2:4:end] .= 4
+
+    condIndSubsetIndex[3:3:end,1:3:end] .= 5
+    condIndSubsetIndex[3:3:end,2:3:end] .= 6
+    condIndSubsetIndex[3:3:end,3:3:end] .= 7
+
+    return Array[findall(vec(condIndSubsetIndex) .==i) for i=1:7]
+
+end
 
 function rand(F::iGMRF)::Vector{<:Real}
 
