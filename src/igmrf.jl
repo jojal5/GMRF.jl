9dcfad40837bffce87207f12c8c1d18cc7829a04
Jonathan Jalbert <39426670+jojal5@users.noreply.github.com>
2026-05-21 09:31:25 -0400

    move and add docsting to fo_nbs()

--- src/igmrf.jl
+++ src/igmrf.jl
@@ -44,36 +44,7 @@ function iGMRF(m₁::Integer, m₂::Integer, order::Integer, κ::Real)::iGMRF
 
 end
 
-function fo_nbs(m₁::Integer, m₂::Integer)::Tuple{Vector{Vector{Int64}}, SparseMatrixCSC{Int64,Int64}}
 
-    # 1-off diagonal elements
-    v = ones(Int64,m₁)
-    v[end] = 0
-    V = repeat(v,outer=m₂)
-    pop!(V)
-
-    # n-off diagonal elements
-    U = ones(Int64,m₁*(m₂-1))
-
-    # get the upper triangular part of the matrix
-    m = m₁ * m₂
-    D = sparse(1:(m-1), 2:m, V, m, m) + sparse(1:(m-m₁),(m₁+1):m, U, m, m)
-
-    # make D symmetric
-    D = D + D'
-
-    # Compute the list of neighbors for each node
-    nbs = fill(Int[], m)
-    for i = 1:m
-        nbs[i] = findall(!iszero, D[:,i])
-    end
-
-    # Put the number of neighbors on the diagonal
-    W = -D + spdiagm(0 => length.(nbs))
-
-    return (nbs, W)
-
-end
 
 function so_nbs(m₁::Integer, m₂::Integer)::Tuple{Vector{Vector{Int64}}, SparseMatrixCSC{Int64,Int64}}
     # Alternative by adding molecules. There should not be missing values in the grid.
