8ea34cf5de925f452ada2119b802fa11902a9df4
Jonathan Jalbert <39426670+jojal5@users.noreply.github.com>
2026-05-24 11:19:25 -0400

    remove Manifest.toml and script.jl from git tracking

--- script.jl
+++ script.jl
@@ -4,20 +4,6 @@ using Pkg
 pkg"activate ."
 
 using GMRF
-import GMRF.second_order_lattice_neighbors
+# import GMRF.second_order_lattice_neighbors
 
-nbs, W = second_order_lattice_neighbors(3, 3)
-
-W_expected = sparse([
-         4 -4  1 -4  2  0  1  0  0
-        -4  9 -4  2 -6  2  0  1  0
-         1 -4  4  0  2 -4  0  0  1
-        -4  2  0  9 -6  1 -4  2  0
-         2 -6  2 -6 16 -6  2 -6  2
-         0  2 -4  1 -6  9  0  2 -4
-         1  0  0 -4  2  0  4 -4  1
-         0  1  0  2 -6  2 -4  9 -4
-         0  0  1  0  2 -4  1 -4  4
-    ])
-
-W == W_expected
+# nbs, W = second_order_lattice_neighbors(3, 3)
