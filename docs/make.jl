3d5677c48d8475d8d4e1a20cfd84ffefe04c67dc
Jonathan Jalbert <39426670+jojal5@users.noreply.github.com>
2026-05-25 15:35:53 -0400

    modify make.jl in docs

--- docs/make.jl
+++ docs/make.jl
@@ -2,14 +2,15 @@ using Documenter
 using GMRF
 
 makedocs(
-    sitename = "GMRF",
+    sitename = "GMRF.jl",
+    modules = [GMRF],
     format = Documenter.HTML(),
-    modules = [GMRF]
+    pages = [
+        "Home" => "index.md",
+    ],
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(
+    repo = "github.com/jojal5/GMRF.jl.git",
+    devbranch = "master",
+)
